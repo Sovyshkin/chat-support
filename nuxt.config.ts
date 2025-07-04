bbbc18da703c02a38ade18b2a15ffae83565c4ba
Sovyshkin <zankov.vadik@yandex.ru>
2025-07-04 17:48:13 +0500

    change nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,15 @@ export default defineNuxtConfig({
     experimental: {
       websocket: true
     },
+    routeRules: {
+      '/**': {
+        cors: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+      }
+    }
   }
 })
--- server/api/users/create.post.ts
+++ server/api/users/create.post.ts
@@ -1,31 +1,59 @@
 import { User } from "~/server/models/user.model";
 import mongoose from "mongoose";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
 // Хеширование пароля
 const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10)
-  return await bcrypt.hash(password, salt)
-}
-
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
 
 export default defineEventHandler(async (event) => {
-    try {
-      await mongoose.connect("mongodb://localhost:27017/Auth");
-      let { email, password, phone, age, name, isGoogleUser, isYandexUser, userId, avatar } = await readBody(event)
-      let passwordHash = await hashPassword(password)
-      console.log(userId, avatar, name);
-      
-        const user = new User({
-            email, password: passwordHash, phone, age, name, isGoogleUser, isYandexUser, userId, avatar
-        });
+  try {
+    await mongoose.connect("mongodb://localhost:27017/Auth");
+    let {
+      email,
+      password,
+      phone,
+      age,
+      name,
+      isGoogleUser,
+      isYandexUser,
+      userId,
+      avatar,
+    } = await readBody(event);
+    let passwordHash = await hashPassword(password);
+    console.log(userId, avatar, name);
 
-        console.log(user);
-        
-        await user.save()
-        return user
-      } catch (e) {
-        console.error(e);
-      }
+    const user = new User({
+      email,
+      password: passwordHash,
+      phone,
+      age,
+      name,
+      isGoogleUser,
+      isYandexUser,
+      userId,
+      avatar,
+    });
+
+    console.log(user);
+
+    await user.save();
+    return user;
+  } catch (e) {
+    console.error(e);
+    if (e.code === 11000) {
+      // Duplicate key error
+      throw createError({
+        statusCode: 409,
+        statusMessage: "User already exists",
+      });
+    }
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Internal Server Error",
+    });
+  }
   return null;
 });
