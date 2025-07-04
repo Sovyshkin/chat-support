7482992bfda358e0fe004cfdadb990648aed78a3
Sovyshkin <zankov.vadik@yandex.ru>
2025-07-04 17:54:02 +0500

    change route

--- server/api/users/create.post.ts
+++ server/api/users/create.post.ts
@@ -1,38 +1,18 @@
 import { User } from "~/server/models/user.model";
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-
-// Хеширование пароля
-const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-};
 
 export default defineEventHandler(async (event) => {
   try {
     await mongoose.connect("mongodb://localhost:27017/Auth");
     let {
-      email,
-      password,
-      phone,
-      age,
       name,
-      isGoogleUser,
-      isYandexUser,
       userId,
       avatar,
     } = await readBody(event);
-    let passwordHash = await hashPassword(password);
     console.log(userId, avatar, name);
 
     const user = new User({
-      email,
-      password: passwordHash,
-      phone,
-      age,
       name,
-      isGoogleUser,
-      isYandexUser,
       userId,
       avatar,
     });
--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -2,12 +2,6 @@ import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema({
     name: String,
-    email: String,
-    password: String,
-    phone: String,
-    age: Number,
-    isGoogleUser: Boolean,
-    isYandexUser: Boolean,
     role: String,
     avatar: String,
     userId: String
